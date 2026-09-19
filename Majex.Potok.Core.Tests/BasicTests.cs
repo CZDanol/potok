45ac0c804f4c85d5756c0181654c89f791fe1e33
spectrolog <5470892+spectrolog@users.noreply.github.com>
2026-09-19 20:35:30 +0200

    The very core idea

---
+++ Majex.Potok.Core.Tests/BasicTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Majex.Potok.Core.Tests;
+
+using Majex.Potok.Core.Dynex;
+
+public class BasicTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var a = new Dynex<float>(() => 3);
+        Assert.Equal(3, a.Eval());
+
+        var b = new Dynex<float>(() => 4);
+        Assert.Equal(4, b.Eval());
+
+        var sum = new Dynex<float>(() => a.Eval() + b.Eval());
+        Assert.Equal(7, sum.Eval());
+    }
+}
---
+++ Majex.Potok.Core/Dynex/Dynex.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+using System.Diagnostics;
+
+namespace Majex.Potok.Core.Dynex;
+
+public class Dynex<T>
+{
+    protected Func<T> _evalFunc;
+    private T? _cachedValue;
+    private bool _isDirty = true;
+
+    public Dynex(Func<T> evalFunc)
+    {
+        _evalFunc = evalFunc;
+    }
+
+    public T? Eval()
+    {
+        Recompute();
+        Debug.Assert(_isDirty == false);
+        return _cachedValue;
+    }
+
+    void Recompute()
+    {
+        if (!_isDirty)
+        {
+            return;
+        }
+
+        _cachedValue = _evalFunc();
+        _isDirty = false;
+    }
+
+}
